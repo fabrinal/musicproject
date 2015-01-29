7bfcbc94200557b1359e975820fc2534cbb93e67
Fabian Rinaldi <code.optica@gmail.com>
2015-01-29 14:52:15 +0800

    add routes to registration

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
   resources :performances
   resources :artists
 
+  get 'registration/index' => 'registration#index'
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
