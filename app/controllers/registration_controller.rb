764add480a59001fbd0dcd869e30b9be7f8b808a
Kintan <kintanayunda@gmail.com>
2015-01-29 15:44:53 +0800

    form created

--- app/controllers/registration_controller.rb
+++ app/controllers/registration_controller.rb
@@ -4,6 +4,29 @@ class RegistrationController < ApplicationController
       format.html
       format.json
     end
+
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def create
+    name = params[:name]
+    Registration.create(item_name:name)
+  end
+
+  def update
+  end
+
+  def destroy
+  end
   end
 
 end
+
+
+  
